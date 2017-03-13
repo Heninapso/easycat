9ab19f71c705a6ef71bfd87ea16ca25911a974ea
Heninapso <heninapso@gmail.com>
2017-03-13 15:33:30 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cat_sitting_offers#index'
+  resources :cat_sitting_offers, only: [:index, :show, :new, :create, :edit, :update, :delete] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:update]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
