4bcc902eccd970b6080510c0389cbff4a4453611
Heninapso <heninapso@gmail.com>
2017-03-13 19:27:56 +0100

    change home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount Attachinary::Engine => "/attachinary"
   devise_for :users
-  root to: 'cat_sitting_offers#index'
+  root 'cat_sitting_offers#index'
   resources :cat_sitting_offers, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :bookings, only: [:new, :create]
   end
