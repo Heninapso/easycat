653b3ddd9dd15923e1e869be9675716cb2a712c5
Heninapso <heninapso@gmail.com>
2017-03-13 18:09:52 +0100

    new, create methods on cso controller ok

--- app/controllers/cat_sitting_offers_controller.rb
+++ app/controllers/cat_sitting_offers_controller.rb
@@ -1,2 +1,41 @@
 class CatSittingOffersController < ApplicationController
+
+  before_action :find_cso, only: [:show, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @catsittingoffer = CatSittingOffer.new
+  end
+
+  def create
+    @catsittingoffer = CatSittingOffer.new(catsittingoffer_params)
+    if @catsittingoffer.save
+      redirect_to cat_sitting_offer_path(@catsittingoffer)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @catsittingoffer.update(catsittingoffer_params)
+  end
+
+  def destroy
+    @catsittingoffer.destroy
+
+  end
+
+
+  private
+
+  def find_cso
+    @catsittingoffer = CatSittingOffer.find(params[:id])
+  end
+
+  def catsittingoffer_params
+    params.require(:cat_sitting_offer).permit(:title, :localisation, :description, :status, photos: [])
+  end
+
 end
