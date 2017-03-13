8260b77a253bcd349a73751ebb98f0d447c653d5
Quentin Coray <quentin.coray@gmail.com>
2017-03-13 15:59:08 +0100

    Created Customers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'customers/show'
+
   devise_for :users
   root to: 'cat_sitting_offers#index'
   resources :cat_sitting_offers, only: [:index, :show, :new, :create, :edit, :update, :delete] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:update]
-  resources :users, only: [:show]
+  resources :customers, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
