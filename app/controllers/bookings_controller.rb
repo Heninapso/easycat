ce9982801958f04300eb79bea9b55c789414d5eb
Heninapso <heninapso@gmail.com>
2017-03-14 09:05:44 +0100

    bugfixxing - follow up

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.save
-    @customer = Customer.find(params(booking[:id])
+    @customer = Customer.find(params(booking[:id]))
     redirect_to cat_sitting_offers_path
   end
 
