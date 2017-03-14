d5a355a0d6a5bc048cd7431aef6ec016ddea2834
Quentin Coray <quentin.coray@gmail.com>
2017-03-14 15:52:39 +0100

    Added booking feature into user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :edit, :update, :show
+  skip_before_action :authenticate_user!, only: :show
 
   def show
     @user = User.find(params[:id])
+    @bookings = current_user.bookings
   end
 
   def edit
