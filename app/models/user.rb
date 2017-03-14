2b6cb13908a8d51229001320799885f405c42cf1
Quentin Coray <quentin.coray@gmail.com>
2017-03-14 13:29:24 +0100

    Fixed booking management and models and db and everything

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :bookings
   has_many :cat_sitting_offers
+  has_attachment :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
