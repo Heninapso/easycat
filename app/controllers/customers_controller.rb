cafb8ad351051941a86eede3e16b55ed1e519f6a
Quentin Coray <quentin.coray@gmail.com>
2017-03-14 10:48:00 +0100

    Fixing conflicts

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,6 @@
 class CustomersController < ApplicationController
-<<<<<<< HEAD
-    skip_before_action :authenticate_user!, only: :show
-=======
   skip_before_action :authenticate_user!, only: :show
->>>>>>> 2d7f93d34d6143929b7831c9f236c1b0b3095072
+
   def show
     @customer = Customer.find(params[:id])
     @cat_sitting_offer = CatSittingOffer.new
