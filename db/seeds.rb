2a020f49dda3975ca5c96ffb14e878c8e66d5cc8
Camille Franceschi <franceschi.camille@gmail.com>
2017-03-16 18:13:44 +0100

    looking

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,19 +40,19 @@ cat_sitting_offer_camille = CatSittingOffer.create(
   {title:"Lovely house for a cat" , localisation: "Paris", description:"Love cat since my childhood", user: camille})
 
 cat_sitting_offer_jean = CatSittingOffer.create(
-  {title:"I really love cats" , localisation: "Paris", description:"Love cat since my childhood", user: jean})
+  {title:"I really love cats" , localisation: "New-York", description:"Love cat since my childhood", user: jean})
 
 cat_sitting_offer_edouard = CatSittingOffer.create(
-  {title:"Looking for a cat friend" , localisation: "Paris", description:"Love cat since my childhood", user: edouard})
+  {title:"Looking for a cat friend" , localisation: "Lyon", description:"Love cat since my childhood", user: edouard})
 
 cat_sitting_offer_alice = CatSittingOffer.create(
-  {title:"I can talk to cats" , localisation: "Paris", description:"Love cat since my childhood", user: alice})
+  {title:"I can talk to cats" , localisation: "Bordeaux", description:"Love cat since my childhood", user: alice})
 
 cat_sitting_offer_quentin1 = CatSittingOffer.create(
-  {title:"Wonderful catsitter" , localisation: "Paris", description:"Love cat since my childhood", user: quentin})
+  {title:"Wonderful catsitter" , localisation: "Dublin", description:"Love cat since my childhood", user: quentin})
 
 cat_sitting_offer_anne_sophie1 = CatSittingOffer.create(
-  {title:"Catsitting is life!" , localisation: "Paris", description:"Love cat since my childhood", user: anne_sophie})
+  {title:"Catsitting is life!" , localisation: "Tokyo", description:"Love cat since my childhood", user: anne_sophie})
 
 cat_sitting_offer_camille1 = CatSittingOffer.create(
   {title:"Lovely house for a cat" , localisation: "Paris", description:"Love cat since my childhood", user: camille})
@@ -61,46 +61,46 @@ cat_sitting_offer_jean1 = CatSittingOffer.create(
   {title:"I really love cats" , localisation: "Paris", description:"Love cat since my childhood", user: jean})
 
 cat_sitting_offer_edouard1 = CatSittingOffer.create(
-  {title:"Looking for a cat friend" , localisation: "Paris", description:"Love cat since my childhood", user: edouard})
+  {title:"Looking for a cat friend" , localisation: "Londres", description:"Love cat since my childhood", user: edouard})
 
 cat_sitting_offer_alice1 = CatSittingOffer.create(
   {title:"I can talk to cats" , localisation: "Paris", description:"Love cat since my childhood", user: alice})
 
 urls1 = [
+  'https://s-media-cache-ak0.pinimg.com/originals/0e/17/71/0e1771711821d502667520aa116b5d3e.jpg',
   'http://chrisleavins.typepad.com/chrisleavins/images/2008/01/12/formal_catportrait_walter_elizabeth.jpg',
-  'http://www.book-a-flat.com/images/paris-salon-2.jpg',
-  'https://s-media-cache-ak0.pinimg.com/originals/0e/17/71/0e1771711821d502667520aa116b5d3e.jpg'
+  'http://www.book-a-flat.com/images/paris-salon-2.jpg'
 ]
 
 urls2 = [
+  'http://www.dog-and-cat-design.com/2802-thickbox_default/griffoir-niche-en-carton-pour-chat-brochhaus-jnr.jpg',
   'https://img.buzzfeed.com/buzzfeed-static/static/2014-09/29/17/enhanced/webdr10/enhanced-2230-1412025361-9.jpg',
-  'http://www.chicroomproperties.com/thumb/property-gallery/items/166/furnished-studio-flat-for-rent-mid-term-in-barcelona-gothic-2.jpg',
-  'http://www.dog-and-cat-design.com/2802-thickbox_default/griffoir-niche-en-carton-pour-chat-brochhaus-jnr.jpg'
+  'http://www.chicroomproperties.com/thumb/property-gallery/items/166/furnished-studio-flat-for-rent-mid-term-in-barcelona-gothic-2.jpg'
 ]
 
 urls3 = [
+  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg',
   'http://justsomething.co/wp-content/uploads/2014/08/men-and-cats-11.jpg',
-  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg',
-  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg'
+  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg'
 
 ]
 
 urls4 = [
+  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg',
   'https://img.buzzfeed.com/buzzfeed-static/static/2014-09/29/15/enhanced/webdr11/enhanced-23445-1412017320-11.jpg',
-  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg',
-  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg'
+  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg'
 ]
 
 urls5 = [
+  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg',
   'https://s-media-cache-ak0.pinimg.com/originals/5e/30/9f/5e309f4b980f7ece8b3346a4025ae0b1.jpg',
-  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg',
-  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg'
+  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg'
 ]
 
 urls6 = [
+  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg',
   'http://www.lifewithcats.tv/wp-content/uploads/2016/07/awkward-5.jpg',
-  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg',
-  'https://archzine.fr/wp-content/uploads/2016/02/niche-pour-chat-maison-pour-chat-galet.jpg'
+  'http://cdn.homedsgn.com/wp-content/uploads/2014/06/Modern-Flat-in-Kyiv-04-850x566.jpg'
 ]
 
 cat_sitting_offer_quentin.photo_urls = urls1
