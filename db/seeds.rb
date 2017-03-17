5c7228ead5edbe1b5f75a2459d61ea6e1ea57e17
Camille Franceschi <franceschi.camille@gmail.com>
2017-03-17 15:53:25 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 Booking.destroy_all
 CatSittingOffer.destroy_all
 User.destroy_all
-quentin = User.create(email: "quentin@gmail.com", password: "azerty")
-anne_sophie = User.create(email: "anne_sophie@gmail.com", password: "azerty")
-camille = User.create(email: "camille@gmail.com", password: "azerty")
-jean = User.create(email: "jean@gmail.com", password: "azerty")
-edouard = User.create(email: "edouard@gmail.com", password: "azerty")
-alice = User.create(email: "alice@gmail.com", password: "azerty")
+quentin = User.create(email: "quentin@gmail.com", password: "azerty", first_name: "Quentin", last_name: "Coray")
+anne_sophie = User.create(email: "anne_sophie@gmail.com", password: "azerty", first_name: "Anne-Sophie", last_name: "Chambon")
+camille = User.create(email: "camille@gmail.com", password: "azerty", first_name: "Camille", last_name: "Franceschi")
+jean = User.create(email: "jean@gmail.com", password: "azerty", first_name: "Jean", last_name: "Flantier")
+edouard = User.create(email: "edouard@gmail.com", password: "azerty", first_name: "Edouard", last_name: "Robert")
+alice = User.create(email: "alice@gmail.com", password: "azerty", first_name: "Alice", last_name: "Martin")
 
 
 photo_quentin = 'https://pbs.twimg.com/profile_images/618004273998249984/-46_Q9Ca.jpg'
@@ -23,7 +23,6 @@ photo_camille = 'https://avatars2.githubusercontent.com/u/21193933?v=3&s=460'
 photo_jean = 'http://cdn77.sadanduseless.com/wp-content/uploads/2015/05/worst9.jpg'
 photo_edouard = 'http://www.goldenmoustache.com/wp-content/uploads/2015/06/worst8.jpg'
 photo_alice = 'http://www.teamjimmyjoe.com/wp-content/uploads/2012/11/Bad-Family-Photos-80s-Yearbook-.jpg'
-
 
 quentin.photo_url = photo_quentin
 anne_sophie.photo_url = photo_anne_sophie
@@ -35,8 +34,7 @@ alice.photo_url = photo_alice
 
 
 
-cat_sitting_offer_quentin = CatSittingOffer.create(
-  {title:"Wonderful catsitter" , localisation: "2 rue de la paix,Paris", description:"Love cat since my childhood", user: quentin})
+cat_sitting_offer_quentin = CatSittingOffer.create({title:"Wonderful catsitter" , localisation: "2 rue de la paix,Paris", description:"Love cat since my childhood", user: quentin})
 
 cat_sitting_offer_quentin1 = CatSittingOffer.create(
   {title:"Wonderful catsitter" , localisation: "6 rue du dragon, Paris", description:"Love cat since my childhood", user: quentin})
@@ -75,38 +73,38 @@ cat_sitting_offer_alice = CatSittingOffer.create(
 cat_sitting_offer_alice1 = CatSittingOffer.create(
   {title:"I can talk to cats" , localisation: "Paris", description:"Love cat since my childhood", user: alice})
 
-url1 = Picture.create(image: '../app/assets/images/flat1.jpg')
-# urls1 = [
-#  assets_path('niche1.jpg'),
-#  '../app/assets/images/flat1.jpg',
-#  '../app/assets/images/people1.jpg'
-# ]
 
-# urls2 = [
-#  '../app/assets/images/niche2.jpg',
-#  '../app/assets/images/flat2.jpg',
-#  '../app/assets/images/people2.jpg'
-# ]
-# urls3 = [
-#  '../app/assets/images/niche3.jpg',
-#  '../app/assets/images/flat3.jpg',
-#  '../app/assets/images/people3.jpg'
-# ]
-# urls4 = [
-#  '../app/assets/images/niche4.jpg',
-#  '../app/assets/images/flat4.jpg',
-#  '../app/assets/images/people4.jpg'
-# ]
-# urls5 = [
-#  '../app/assets/images/niche5.jpg',
-#  '../app/assets/images/flat5.jpg',
-#  '../app/assets/images/people5.jpg'
-# ]
-# urls6 = [
-#  '../app/assets/images/niche6.jpg',
-#  '../app/assets/images/flat6.jpg',
-#  '../app/assets/images/people6.jpg'
-# ]
+urls1 = [
+  'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/people1_hhhztk.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/niche1_aebqn2.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760655/flat1_hnuwjy.jpg'
+]
+
+urls2 = [
+  'http://res.cloudinary.com/dymvgezcn/image/upload/v1489761803/people2_ai5mgs.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/niche2_hixgiu.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760655/flat2_xwjied.jpg'
+]
+urls3 = [
+ 'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/people3_qkvhli.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/niche3_gdy5bm.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760655/flat3_nokpee.jpg'
+]
+urls4 = [
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/people4_foqrsx.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/niche4_pxyatx.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760655/flat4_vcxs8k.jpg'
+]
+urls5 = [
+  'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/people5_eotzug.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/niche5_rostxe.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760655/flat5_yftry0.jpg'
+]
+urls6 = [
+  'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/people6_yw8jwe.jpg',
+ 'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760656/niche6_klnqyf.jpg',
+'http://res.cloudinary.com/dymvgezcn/image/upload/v1489760655/flat6_ulurbx.jpg'
+]
 
 cat_sitting_offer_quentin.photo_urls = urls1
 cat_sitting_offer_anne_sophie.photo_urls = urls2
